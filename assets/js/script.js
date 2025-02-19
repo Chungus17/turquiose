5cee962c2fc598b8dfc6c365b17cb75e24965e60
Chungus17 <haroon.n.shah1999@gmail.com>
2025-02-19 10:46:48 +0300

    From submission email

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 "use strict";
 
+window.onload = function() {
+  // Reset the form fields when the page loads
+  document.getElementById("myForm").reset();
+};
+
 /* ADD Event Listener on Multiple Elements */
 const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
@@ -192,4 +197,45 @@ document.addEventListener("DOMContentLoaded", () => {
 
   window.addEventListener("scroll", handleScroll);
   handleScroll(); // Trigger check on initial load
+});
+
+
+
+const form = document.getElementById('myForm');
+const result = document.getElementById('result');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  const object = Object.fromEntries(formData);
+  const json = JSON.stringify(object);
+  result.innerHTML = "Please wait..."
+
+    fetch('https://api.web3forms.com/submit', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: json
+        })
+        .then(async (response) => {
+            let json = await response.json();
+            if (response.status == 200) {
+                result.innerHTML = json.message;
+            } else {
+                console.log(response);
+                result.innerHTML = json.message;
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            result.innerHTML = "Something went wrong!";
+        })
+        .then(function() {
+            form.reset();
+            setTimeout(() => {
+                result.style.display = "none";
+            }, 3000);
+        });
 });
